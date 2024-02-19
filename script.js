3bf121b661657b57eb6d5fae4db190b7eee2da17
EverAlexi <160448500+EverAlexi@users.noreply.github.com>
2024-02-19 15:25:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('BOTON').addEventListener('click', function() {
+    swal({
+        title: "!ENTENDIDO!",
+        text: "!YO TAMBIEN TE AMO Y TE SEGUIRE AMANDO SIN IMPORTAR LO QUE PASE!",
+        icon: "info",
+        button: "¡GRACIAAS!",
+        dangerMode: true,
+    });
+});
